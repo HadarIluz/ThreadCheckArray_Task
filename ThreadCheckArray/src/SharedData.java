b566061930d4c641f8f9e06e486256dd59a42b89
Yuval Hayam <12692573+yuval96@users.noreply.github.com>
2021-03-24 09:46:07 +0200

    add JavaDoc to SharedData

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,45 +1,82 @@
 import java.util.ArrayList;
 
+/**
+ * @author yuval
+ *
+ */
 public class SharedData 
 {
-	//Sol: int[] array change to ArrayList<Integer> array.
+	/**
+	 * array - the array where we are searching for the sum
+	 * winArray - the subarray where we found the sum
+	 * flag - indicate if we found the sum
+	 * b - the sum we are searching
+	 */
 	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
-	//change111
 	
+	/** the constructor defines two parameters
+	 * 
+	 * @param array		ArrayList of numbers
+	 * @param b			sum we want to find in array
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**
+	 * 
+	 * @return the winning array
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/**
+	 * 
+	 * @param winArray		the winning array we want to set
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * 
+	 * @return array
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * 
+	 * @return b
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * 
+	 * @return flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * 
+	 * @param flag		set the flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
